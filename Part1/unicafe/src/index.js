a063c11638e06a02ad118a74ec0846afadb8bf44
Martin <martinsantaclara@gmail.com>
2021-12-11 10:01:17 -0300

    exercise 1.7

--- Part1/unicafe/src/index.js
+++ Part1/unicafe/src/index.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
 
 const App = () => {
   // save clicks of each button to its own state
@@ -11,6 +11,10 @@ const App = () => {
   const setToNeutral = () => setNeutral(neutral+1)
   const setToBad = () => setBad(bad+1)
 
+  const total = good + neutral + bad    
+  const average = total!==0 ? (good-bad)/total : 0
+  const positive = (total!==0 ? good/total : 0) * 100
+  
   return (
     <div>
       <h1>give feedback</h1>
@@ -21,6 +25,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
     </div>
   )
 }
